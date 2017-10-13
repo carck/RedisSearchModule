dd5bb1a67119b11739e072dceaa8e8accf4e6523
lance.zhou <carck@foxmail.com>
2017-10-13 09:43:01 +0800

    Return total elements

--- module/module.c
+++ module/module.c
@@ -20,25 +20,25 @@ typedef struct {
   cJSON *json;
   cJSON *sort;
   RedisModuleString *reply;
-} Ext;
+} Entity;
 
 int compare(void *arg, const void *a, const void *b) {
-  Ext *ext1 = *(Ext **)a;
-  Ext *ext2 = *((Ext **)b);
-  int sort= *(int*)arg;
+  Entity *ext1 = *(Entity **)a;
+  Entity *ext2 = *((Entity **)b);
+  int sort = *(int *)arg;
   cJSON *pin1 = ext1->sort;
   cJSON *pin2 = ext2->sort;
   return sort * strcmp(pin1->valuestring, pin2->valuestring);
 }
 
-void FreeArgv(RedisModuleCtx *ctx, RedisModuleString **argv, int argc){
+void FreeArgv(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   for (int i = 0; i < argc; i++) {
     RedisModule_FreeString(ctx, argv[i]);
   }
   RedisModule_Free(argv);
 }
 
-void *do_search(void *arg) {
+void *DoSearch(void *arg) {
   CommandCtx *cctx = arg;
   RedisModuleBlockedClient *bc = cctx->bc;
   RedisModuleString **argv = cctx->argv;
@@ -59,48 +59,48 @@ void *do_search(void *arg) {
 
   // get the current value of the hash element
   RedisModule_ThreadSafeContextLock(ctx);
-  RedisModuleCallReply *rep = RedisModule_Call(ctx, "HVALS", "s", argv[1]);
+  RedisModuleCallReply *reply = RedisModule_Call(ctx, "HVALS", "s", argv[1]);
   RedisModule_ThreadSafeContextUnlock(ctx);
 
-  if (rep == NULL) {
+  if (reply == NULL) {
     RedisModule_ReplyWithError(ctx, "ERR reply is NULL");
     goto free_argv;
-  } else if (RedisModule_CallReplyType(rep) == REDISMODULE_REPLY_ERROR) {
-    RedisModule_ReplyWithCallReply(ctx, rep);
+  } else if (RedisModule_CallReplyType(reply) == REDISMODULE_REPLY_ERROR) {
+    RedisModule_ReplyWithCallReply(ctx, reply);
     goto free_reply;
   }
 
-  size_t replyCount = RedisModule_CallReplyLength(rep);
+  size_t ct_reply = RedisModule_CallReplyLength(reply);
 
   const char *filter = RedisModule_StringPtrLen(argv[2], NULL);
   const char *sortName = RedisModule_StringPtrLen(argv[3], NULL);
-  Vector *res = NewVector(Ext *, 200);
+  Vector *res = NewVector(Entity *, 200);
 
-  Ext *ext;
+  Entity *ext;
   cJSON *json_root;
   cJSON *json_value;
-  RedisModuleCallReply *cursor;
+  RedisModuleCallReply *cr_ext;
   RedisModuleString *json_body;
   int found;
   char *fields[] = {"name", "department", "pin"};
-  for (int i = 0; i < replyCount; i++) {
-    cursor = RedisModule_CallReplyArrayElement(rep, i);
-    json_body = RedisModule_CreateStringFromCallReply(cursor);
+  for (int i = 0; i < ct_reply; i++) {
+    cr_ext = RedisModule_CallReplyArrayElement(reply, i);
+    json_body = RedisModule_CreateStringFromCallReply(cr_ext);
     json_root = cJSON_Parse(RedisModule_StringPtrLen(json_body, NULL));
 
     found = 0;
     for (int j = 0; j < 3; j++) {
       json_value = cJSON_GetObjectItem(json_root, fields[j]);
-      //strstr case sensitive
+      // strstr case sensitive
       if (strcasestr(json_value->valuestring, filter)) {
         found = 1;
         break;
       }
     }
     if (found == 1) {
-      ext = RedisModule_Alloc(sizeof(Ext));
+      ext = RedisModule_Alloc(sizeof(Entity));
       ext->json = json_root;
-      ext->sort = cJSON_GetObjectItem(json_root, sortName);;
+      ext->sort = cJSON_GetObjectItem(json_root, sortName);
       ext->reply = json_body;
       Vector_Push(res, ext);
       ext = NULL;
@@ -122,8 +122,9 @@ void *do_search(void *arg) {
   }
 
   if (Vector_Size(res) > 0) {
-    Ext *ext2;
-    RedisModule_ReplyWithArray(ctx, min(Vector_Size(res), page_end) - page_start);
+    Entity *ext2;
+    RedisModule_ReplyWithArray(ctx, min(Vector_Size(res), page_end) - page_start + 1);
+    RedisModule_ReplyWithDouble(ctx, Vector_Size(res));
     for (size_t idx = 0; idx < Vector_Size(res); idx++) {
       Vector_Get(res, idx, &ext2);
       cJSON_Delete(ext2->json);
@@ -139,7 +140,7 @@ void *do_search(void *arg) {
 
   Vector_Free(res);
 free_reply:
-  RedisModule_FreeCallReply(rep);
+  RedisModule_FreeCallReply(reply);
 free_argv:
   FreeArgv(ctx, argv, argc);
   RedisModule_FreeThreadSafeContext(ctx);
@@ -159,7 +160,7 @@ int HSearchCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
   RedisModuleBlockedClient *bc = RedisModule_BlockClient(ctx, NULL, NULL, NULL, 0);
 
-  //copy argv to use in thread
+  // copy argv to use in thread
   RedisModuleString **argvSafe = RedisModule_Alloc(sizeof(RedisModuleString *) * argc);
   for (int i = 0; i < argc; i++) {
     argvSafe[i] = RedisModule_CreateStringFromString(ctx, argv[i]);
@@ -170,7 +171,7 @@ int HSearchCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   cctx->argv = argvSafe;
   cctx->argc = argc;
 
-  if (tpool_add_work(do_search, (void *)cctx) != 0) {
+  if (tpool_add_work(DoSearch, (void *)cctx) != 0) {
     RedisModule_AbortBlock(bc);
     RedisModule_ReplyWithError(ctx, "Sorry can't create a thread");
   }
